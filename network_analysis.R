48db58185f781c70980be34730d5d700fdff35d3
ShirNehoray <shirneho@post.bgu.ac.il>
2025-09-02 15:52:53 +0300

    Update network_analysis.R

--- network_analysis.R
+++ network_analysis.R
@@ -32,3 +32,8 @@ ggplot(metrics_long, aes(x = Site, y = Value, fill = Metric)) +
   labs(title = "Network metrics per site", x = "Site", y = "Value") +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+
+
+
+
